2760ce1ddcfd33bcdc614381dbcbd8e5fc6bd4ae
yokoyama <s56240ky@kurume.kosen-ac.jp>
2021-01-19 14:21:15 +0900

    正規化削除

--- def_yokoyama.py
+++ def_yokoyama.py
@@ -25,15 +25,15 @@ def D_to_df(D, N, d_size):
     return df
 
 
-def D_to_df_kai(D, N, M, d_size):
-    dd = d_size*d_size
-    d = np.zeros(N*M)
-    df = np.zeros(N*M, dtype=np.complex)
-    for i in range(M):
-        flat = np.ravel(D[i])
-        d[i*N:(i+1)*N] = np.pad(flat, (0, N-dd))
-        df[i*N:(i+1)*N] = np.fft.fft(d[i*N:(i+1)*N])
-    return d, df
+# def D_to_df_kai(D, N, M, d_size):
+#     dd = d_size*d_size
+#     d = np.zeros(N*M)
+#     df = np.zeros(N*M, dtype=np.complex)
+#     for i in range(M):
+#         flat = np.ravel(D[i])
+#         d[i*N:(i+1)*N] = np.pad(flat, (0, N-dd))
+#         df[i*N:(i+1)*N] = np.fft.fft(d[i*N:(i+1)*N])
+#     return d, df
 
 
 def D_to_d(D, N, d_size):
@@ -64,12 +64,12 @@ def make_IDDt(df, N, M, alpha):
 def make_migi(IDDt, df, byf, N, M, alpha):
     Dtb = np.zeros(N*M, dtype=np.complex)
     migi_NM = np.zeros(N*M)
-    migi_N = np.fft.ifft(byf/IDDt)*np.sqrt(N)
+    migi_N = np.fft.ifft(byf/IDDt)
     migi_N = migi_N.astype(np.float64)
     for i in range(M):
         Dtb[i*N:(i+1)*N] = df[i*N:(i+1)*N].conjugate()/IDDt*byf*alpha*-1
     for i in range(M):
-        migi_NM[i*N:(i+1)*N] = np.fft.ifft(Dtb[i*N:(i+1)*N])*np.sqrt(N)
+        migi_NM[i*N:(i+1)*N] = np.fft.ifft(Dtb[i*N:(i+1)*N])
     migi_NM = migi_NM.astype(np.float64)
     return np.concatenate([migi_NM, migi_N], 0)
 
@@ -83,19 +83,19 @@ def make_hidari(IDDt, df, bxf, N, M, alpha):
     for i in range(M):
         DtDb[i*N:(i+1)*N] = df[i*N:(i+1)*N].conjugate()*hidari_N
     DtDb = bxf - (alpha)*DtDb
-    hidari_N = np.fft.ifft(hidari_N)*np.sqrt(N)
+    hidari_N = np.fft.ifft(hidari_N)
     hidari_NM = np.zeros(N*M)
     for i in range(M):
-        hidari_NM[i*N:(i+1)*N] = np.fft.ifft(DtDb[i*N:(i+1)*N])*np.sqrt(N)
+        hidari_NM[i*N:(i+1)*N] = np.fft.ifft(DtDb[i*N:(i+1)*N])
     hidari_N = hidari_N.astype(np.float64)
     hidari_NM = hidari_NM.astype(np.float64)
     return np.concatenate([hidari_NM, hidari_N], 0)
 
 
-def resolvent_b(df, xy, s, rho, lamd, N, M):
-    x = pr.prox_l1(xy[:N*M], lamd/rho)
-    y = xy[N*M:] - (pr.prox_l1(xy[N*M:] - s, 1/rho) + s)
-    return np.concatenate([x, y], 0)
+# def resolvent_b(df, xy, s, rho, lamd, N, M):
+#     x = pr.prox_l1(xy[:N*M], lamd/rho)
+#     y = xy[N*M:] - (pr.prox_l1(xy[N*M:] - s, 1/rho) + s)
+#     return np.concatenate([x, y], 0)
 
 
 def make_y(df, x, N, M):
@@ -104,11 +104,11 @@ def make_y(df, x, N, M):
     # print("x.shape :", x.shape)
     xf = np.zeros(x.shape, dtype=np.complex)
     for i in range(M):
-        xf[i*N:(i+1)*N] = np.fft.fft(x[i*N:(i+1)*N])/np.sqrt(N)
+        xf[i*N:(i+1)*N] = np.fft.fft(x[i*N:(i+1)*N])
     yf = np.zeros(N, dtype=np.complex)
     for i in range(M):
         yf = df[i*N:(i+1)*N]*xf[i*N:(i+1)*N] + yf
-    y = np.fft.ifft(yf)*np.sqrt(N)
+    y = np.fft.ifft(yf)
     y = y.astype(np.float64)
     return y
 
@@ -121,7 +121,7 @@ def D_to_dd_conj(D, dy, xf, N, d_size):
     # emptyの方がメモリ抑えられるかも
     df = np.zeros(N*N*D.shape[0], dtype=np.complex)
     for i in range(D.shape[0]):
-        df[i*N:(i+1)*N] = np.fft.fft(flat[i*N:(i+1)*N])/np.sqrt(N)
+        df[i*N:(i+1)*N] = np.fft.fft(flat[i*N:(i+1)*N])
     # yf = np.zeros(N, dtype=np.complex)
     # for i in range(D.shape[0]):
     #     yf = df[i*N:(i+1)*N]*xf[i*N:(i+1)*N] + yf
@@ -149,8 +149,8 @@ def coefficient_learning(D, x, y, s, N, M, rho, lamd, d_size, ite=200):
             by = 2 * pr_y - y[j]
             bxf = np.zeros(bx.shape, dtype=np.complex)
             for i in range(M):
-                bxf[i*N:(i+1)*N] = np.fft.fft(bx[i*N:(i+1)*N])/np.sqrt(N)
-            byf = np.fft.fft(by)/np.sqrt(N)
+                bxf[i*N:(i+1)*N] = np.fft.fft(bx[i*N:(i+1)*N])
+            byf = np.fft.fft(by)
             IDDt = make_IDDt(df, N, M, rho)
             hidari = make_hidari(IDDt, df, bxf, N, M, rho)
             migi = make_migi(IDDt, df, byf, N, M, rho)
@@ -207,8 +207,7 @@ def pr_d(d, N, M, d_size):
     d = d.reshape(M, rootN, rootN)
     d = d.transpose(1, 2, 0)
     d_Pcn = cnvrep.Pcn(d.reshape(rootN, rootN, 1, 1, M), (d_size, d_size, M), Nv=(rootN, rootN)).squeeze()
-    d_Pcn = d_Pcn.transpose(2, 0, 1)
-    d = d_Pcn.reshape(N*M)
+    d = d_Pcn.transpose(2, 0, 1)
     return d
 
 
@@ -227,7 +226,7 @@ def dictinary_learning(D, dcon, xf, s, N, M, rho, d_size, ite=50):
             b = 2 * re_d - dd_conj[j]
             bf = np.zeros(b.shape, dtype=np.complex)
             for k in range(M+1):
-                bf[k*N:(k+1)*N] = np.fft.fft(b[k*N:(k+1)*N])/np.sqrt(N)
+                bf[k*N:(k+1)*N] = np.fft.fft(b[k*N:(k+1)*N])
             IXXt = make_IDDt(xf[j], N, M, rho)
             hidari = make_hidari(IXXt, xf[j], bf[:N*M], N, M, rho)
             migi = make_migi(IXXt, xf[j], bf[N*M:], N, M, rho)
@@ -263,8 +262,8 @@ def reconstruct(xf, d, N, M):
     for i in range(M):
         df[i*N:(i+1)*N] = np.fft.fft(d[i*N:(i+1)*N])
     for i in range(M):
-        sf = xf[i*N:(i+1)*N]/np.sqrt(N) * df[i*N:(i+1)*N] + sf
-    s = np.fft.ifft(sf)*N
+        sf = xf[i*N:(i+1)*N] * df[i*N:(i+1)*N] + sf
+    s = np.fft.ifft(sf)
     s = s.astype(np.float64)
     sqrtN = int(np.sqrt(N))
     return s.reshape(sqrtN, sqrtN)
@@ -293,7 +292,7 @@ def l1x(X, N, M):
         
 
 # %%
-exim = util.ExampleImages(scaled=True, zoom=0.1, gray=True)
+exim = util.ExampleImages(scaled=True, zoom=0.25, gray=True)
 S1 = exim.image('barbara.png', idxexp=np.s_[10:522, 100:612])
 S2 = exim.image('kodim23.png', idxexp=np.s_[:, 60:572])
 S3 = exim.image('monarch.png', idxexp=np.s_[:, 160:672])
@@ -322,11 +321,6 @@ print(S1.shape)
 M = 16
 d_size = 9
 # proxのstep
-rhox = 0.01
-rhod = 0.01
-lamd = 0.001
-coef_ite = 200
-dict_ite = 200
 # D = util.convdicts()['G:12x12x36']
 # D = D.transpose(2, 0, 1)
 K = len(S)
@@ -350,22 +344,24 @@ dud_log = []
 hizero = []
 goal = []
 # %%
-for i in range(5):
+rhox = 1
+rhod = 1
+lamd = 0.05
+coef_ite = 3
+dict_ite = 1
+total_ite = 200
+# %%
+for i in range(total_ite):
     roopcount = roopcount + 1
-    X, y, prx, dux, hize = coefficient_learning(d, X, y, S, N*N, M, rhox, lamd, d_size, ite= 500)
-    prx_log.append(prx[0])
-    dux_log.append(dux[0])
-    hizero.append(hize[1])
+    X, y, prx, dux, hize = coefficient_learning(d, X, y, S, N*N, M, rhox, lamd, d_size, coef_ite)
+    prx_log.append(prx[coef_ite-1])
+    dux_log.append(dux[coef_ite-1])
+    hizero.append(hize[coef_ite-1])
     xf = X_to_xf(X, N*N, M)
-    # d,df  = D_to_df_kai(D, N*N, M, d_size)
-    # d = D_to_d(D, N*N, d_size)
-    d, dcon, prd, dud = dictinary_learning(d, dcon, xf, S, N*N, M, rhod, d_size, ite = 500)
+    d, dcon, prd, dud = dictinary_learning(d, dcon, xf, S, N*N, M, rhod, d_size, dict_ite)
     prd_log.append(prd[0])
     dud_log.append(dud[0])
-    d = d.reshape(M, N, N)
-    d = d.transpose(1, 2, 0)
-    d_Pcn = cnvrep.Pcn(d.reshape(N, N, 1, 1, M), (d_size, d_size, M), Nv=(N, N)).squeeze()
-    d = d_Pcn.transpose(2, 0, 1)
+    d = pr_d(d, N*N, M, d_size)
     # X = X.transpose(1, 2, 0)
     d = d[:, :d_size, :d_size]
     d = d.transpose(1, 2, 0)
@@ -376,7 +372,7 @@ for i in range(5):
     D = D_to_d(d, N*N, d_size)
     image = reconstruct(xf[0], D, N*N, M)
     print("iterate number: ", roopcount)
-    imgr = sl1 + image/N*10
+    imgr = sl1 + image
     print("Reconstruction PSNR: %.2fdB\n" % sm.psnr(ori[0], imgr))
     # fig = plot.figure(figsize=(14, 7))
     # plot.subplot(1, 2, 1)
@@ -391,23 +387,20 @@ for i in range(5):
     #     plot.subplot(1, 2, 2)
     #     plot.imview(image, title='Reconstructed['+str(i+1)+"]", fig=fig)
 # %%
-print("iterate number: ", roopcount)
-imgr = sl1 + image*sh1[0][0]/image[0][0]
-print("Reconstruction PSNR: %.2fdB\n" % sm.psnr(ori[0], imgr))
-# %%
 print(sh1)
 # aaa = sh1.reshape(N*N)
 # print(np.linalg.norm(aaa, ord=1))
 print(image)
+imgr = sl1 + image
 fig = plot.figure(figsize=(14, 7))
 plot.subplot(1, 2, 1)
 plot.imview(ori[0], title='Original', fig=fig)
 plot.subplot(1, 2, 2)
-plot.imview(imgr, title='Reconstructed['+str(i+1)+"]", fig=fig)
+plot.imview(imgr, title='Reconstructed['+str(i+1)+"]"+str(sm.psnr(ori[0], imgr)), fig=fig)
 # %%
 # fig = plt.figure()
-x_coef = np.arange(5)
-x_dict = np.arange(5)
+x_coef = np.arange(total_ite)
+x_dict = np.arange(total_ite)
 # ax1 = fig.add_subplot(1, 2, 1)
 # ax2 = fig.add_subplot(1, 2, 2)
 ax=plt.subplot()
@@ -417,22 +410,24 @@ plt.plot(x_coef, prx_log, color = "b", label = "X Primal")
 plt.plot(x_coef, dux_log, color = "g", label = "X Dual")
 plt.plot(x_dict, prd_log, color = "r", label = "D Primal")
 plt.plot(x_dict, dud_log, color = "c", label = "D Dual")
-plt.title("x5 d10 rho0.01 lamd0.001: "+str(roopcount))
+plt.title("x"+str(coef_ite)+"d"+str(dict_ite)+"lambd"+str(lamd))
 plt.xlabel("Iterations")
 plt.ylabel("Residual")
 plt.legend()
-# %%
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 ax1.plot(x_coef, hizero)
 ax2.plot(x_coef, goal)
+plt.title("x"+str(coef_ite)+"d"+str(dict_ite)+"lambd"+str(lamd))
+plt.legend()
 # %%
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(1, 2, 2)
-ax1.plot(x_coef[10:], coef_log[10:], marker="o", color = "red", linestyle = "--")
-ax2.plot(x_dict[10:], dict_log[10:], marker="v", color = "blue", linestyle = ":")
+plt.plot(x_coef[25:], prx_log[25:], color = "b", label = "X Primal")
+plt.plot(x_dict[25:], prd_log[25:], color = "r", label = "D Primal")
+plt.title("x"+str(coef_ite)+"d"+str(dict_ite)+"lambd"+str(lamd)+"roop"+str(roopcount))
+plt.xlabel("Iterations")
+plt.ylabel("Residual")
+plt.legend()
 # %%
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1)
